01821bcd2bf67769364288fa1fe38f9ec1e32182
expositor <expositor@users.noreply.github.com>
2014-03-03 11:57:41 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password passwor
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
 # Install PhpMyAdmin
-sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/dbconfig-install boolean true'
-sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/app-password-confirm password root'
-sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/admin-pass password root'
-sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/app-pass password root'
-sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2'﻿
+# sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/dbconfig-install boolean true'
+# sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/app-password-confirm password root'
+# sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/admin-pass password root'
+# sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/app-pass password root'
+# sudo debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2'﻿
 
 # Install Ubuntu PhP and Curl
 sudo apt-get install -y vim curl python-software-properties
@@ -19,7 +19,7 @@ sudo apt-get update
 
 # Install Web Packages
 sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-readline mysql-server-5.5 php5-mysql git-core php5-xdebug
-sudo apt-get install -y phpmyadmin
+# sudo apt-get install -y phpmyadmin
 
 # Install xdebug
 cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
@@ -37,24 +37,24 @@ sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 sed -i "s/disable_functions = .*/disable_functions = /" /etc/php5/cli/php.ini
 
 # Aliases
-echo "alias laravelcustomize='curl -L -o install.sh https://raw.github.com/expositor/LaravelCustomize/master/install.sh && chmod +x install.sh && ./install.sh && rm install.sh'" >> /home/vagrant/.bashrc
-source ~/.bashrc
+# echo "alias laravelcustomize='curl -L -o install.sh https://raw.github.com/expositor/LaravelCustomize/master/install.sh && chmod +x install.sh && ./install.sh && rm install.sh'" >> /home/vagrant/.bashrc
+# source ~/.bashrc
 
 # Restart Apache
 sudo service apache2 restart
 
 # Apache Configuration
-sudo sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
-sudo sh -c 'echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf'
-sudo sh -c 'echo "ServerName localhost" >> /etc/apache2/apache2.conf'
+# sudo sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+# sudo sh -c 'echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf'
+# sudo sh -c 'echo "ServerName localhost" >> /etc/apache2/apache2.conf'
 
 # Graceful Restart of Apache
-/etc/init.d/apache2 reload
+# /etc/init.d/apache2 reload
 
 # Get Composer
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
 # Get Laravel
-cd /vagrant
-composer create-project laravel/laravel --prefer-dist
+# cd /vagrant
+# composer create-project laravel/laravel --prefer-dist
